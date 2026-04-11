beec50340a7124a9e9a225f0ba8c140feac4be5a
Lemuel Vincent Alcoseba III <a.lemuelvincent@gmail.com>
2026-04-11 15:38:45 +0800

    update the title and add the icon

--- src/main/java/com/example/csit228capstone/HelloApplication.java
+++ src/main/java/com/example/csit228capstone/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.csit228capstone;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("RecordsForm.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Image icon = new Image(getClass().getResourceAsStream("/com/example/images/logo.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("LIGTAS-Brgy");
         stage.setScene(scene);
         stage.show();
     }
