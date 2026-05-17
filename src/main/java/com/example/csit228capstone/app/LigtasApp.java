4f59b1ede2576579fc090c75c7ae0ab94f6c9363
Daniel Duarte Jr <danielduartejr012005@gmail.com>
2026-05-17 21:41:12 +0800

    fixes app icon and css

--- src/main/java/com/example/csit228capstone/app/LigtasApp.java
+++ src/main/java/com/example/csit228capstone/app/LigtasApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import com.example.csit228capstone.util.SupabaseConnectionManager;
 
@@ -11,7 +12,6 @@ public class LigtasApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Initialize DB connection pool
         SupabaseConnectionManager.getInstance();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/csit228capstone/login/Login.fxml"));
@@ -22,6 +22,17 @@ public class LigtasApp extends Application {
 
         primaryStage.setTitle("LIGTAS-Brgy — Barangay Command Center");
         primaryStage.setScene(scene);
+
+
+        try {
+            Image icon = new Image(
+                    getClass().getResourceAsStream("/images/ligtas-brgy-logo.png")
+            );
+            primaryStage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.err.println("App icon not found: " + e.getMessage());
+        }
+
 //        primaryStage.setMinWidth(1200);
 //        primaryStage.setMinHeight(800);
 //        primaryStage.setFullScreen(true);
